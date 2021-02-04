ee6818e25fb6729e76356905998ed6552024c08a
Emma Idowu <emmaisdivine@gmail.com>
2021-02-04 06:04:20 +0100

    array challenge update

--- arrays/expense-tracker.js
+++ arrays/expense-tracker.js
@@ -10,20 +10,36 @@ const account = {
         description: 'Sunday School Offering',
         amount: 8500000
     }*/],
+    income: [],
     addExpense: function (expense, amount) {
-        account.expenses.push({
+        this.expenses.push({
             description: expense,
+            amount: amount
+        })
+    },
+    addIncome: function (income, amount) {
+        this.income.push({
+            description: income,
             amount: amount
         })
     },
     getAccountSummary: function () {  //you can put the account name as argument, it makes no difference
         let totalExpenses = 0
+        let totalIncome = 0
+        let balance = 0
+
         this.expenses.forEach(function (expense, index) {
             totalExpenses = expense.amount + totalExpenses
             return totalExpenses
         })
 
-        return `${this.name} has $${totalExpenses} in expenses`
+        this.income.forEach(function (income, index) {
+            totalIncome = income.amount + totalIncome
+            return totalExpenses
+        })
+        accountBalance = totalIncome - totalExpenses
+
+        return `${this.name} has a balance of $${accountBalance}. $${totalIncome} in income. $${totalExpenses} in expenses`
     }
 }
 
@@ -52,4 +68,5 @@ const account = {
 
 account.addExpense('Rent', 950)
 account.addExpense('Coffee', 2)
+account.addIncome('Job', 1000)
 console.log(account.getAccountSummary())
