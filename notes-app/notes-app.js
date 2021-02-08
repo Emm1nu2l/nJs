45db57824777041c47819a6aec9ab7bc35d027ea
Emma Idowu <emmaisdivine@gmail.com>
2021-02-08 11:06:51 +0100

    notes-app.js added

---
+++ notes-app/notes-app.js
@@ -0,0 +1,17 @@
+// DOM - Document Object Model
+
+// Query and remove
+// const p = document.querySelector('p')
+// p.remove() 
+
+//remove() removes the tags. also, querySelector matches the first type
+//but to get all you use querySelectorAll
+
+// Query all and remove
+
+const ps = document.querySelectorAll('p')
+
+ps.forEach(function(p) {
+   // p.remove()
+   console.log(p.textContent) //textContent is used to print the content of the html tag/type
+})
